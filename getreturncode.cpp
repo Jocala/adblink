42b7bd1b6eae1a894d83b4c3b07e30ed41ffdb1b
jocala <jocala@jocala.com>
2026-05-29 13:36:12 -0400

    CMakeLists.txt edit. getreturncode.cpp busybox detection bug fixed

--- getreturncode.cpp
+++ getreturncode.cpp
@@ -1,5 +1,6 @@
 #include "getreturncode.h"
 
+#include "adbutils.h"
 #include "logfile.h"
 #include <QDebug>
 #include <QProcess>
@@ -7,30 +8,18 @@
 
 bool getreturncode(const QString &cstring)
 {
-
-
-
-
+  QString program = getadbpath();
+  QStringList args = QProcess::splitCommand(cstring);
+  args.removeFirst();
 
   QProcess run_command;
   run_command.setProcessChannelMode(QProcess::MergedChannels);
-  run_command.start(cstring);
+  run_command.start(program, args);
 
   run_command.waitForStarted();
-
 
   while(run_command.state() != QProcess::NotRunning)
       qApp->processEvents();
 
-  QString command=run_command.readAll();
-
-
- bool success = (run_command.exitCode() == 0);
-
-// if (!success)
-//    { logfile("ERROR: "+cstring);
-//      logfile("ERROR: "+command); }
-
-  return success;
-
+  return (run_command.exitCode() == 0);
 }
