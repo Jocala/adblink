5286152cd0f32c6eaff31cee34e272a85efdffcd
jocala <jocala@jocala.com>
2026-06-18 11:40:08 -0400

    Revert "add codesign-macos.cmake CPack pre-build script"
    This reverts commit 801232e686c042be65ce68aa0d42ebe4d83bb174.

--- packaging/codesign-macos.cmake
+++
@@ -1,9 +0,0 @@
-execute_process(
-    COMMAND codesign --force --deep --sign -
-        "${CPACK_TEMPORARY_DIRECTORY}/adblink.app"
-    RESULT_VARIABLE result
-)
-if(NOT result EQUAL 0)
-    message(FATAL_ERROR "codesign failed: ${result}")
-endif()
-message(STATUS "Re-signed adblink.app after CPack modifications")
